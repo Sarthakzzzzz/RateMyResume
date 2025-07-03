bb73c1a05ccb087d127a61d58f71eea192755685
sarthakzzzzz <sarthakpujari1970@gmail.com>
2025-07-03 18:05:20 +0530

    TestCases Added

---
+++ App/tests.py
@@ -0,0 +1,18 @@
+from django.test import SimpleTestCase
+
+
+class testUrls(SimpleTestCase):
+    def test_home_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_score_url(self):
+        response = self.client.get('/score/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'score.html')
+
+    def test_admin_url(self):
+        response = self.client.get('/admin/')
+        # Redirect to login page if not logged in
+        self.assertEqual(response.status_code, 302)
